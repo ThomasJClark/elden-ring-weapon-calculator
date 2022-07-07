6d2b0555050f109a3a31cf2664232bdfe0601b44
Tom Clark <the.thomas.j.clark@gmail.com>
2022-07-07 15:30:09 -0400

    Use .js extension for weapon data so Cloudflare will cache it

--- src/app/useWeapons.tsx
+++ src/app/useWeapons.tsx
@@ -7,7 +7,7 @@ export default function useWeapons() {
   const [weapons, setWeapons] = useState(new Map<string, Weapon>());
 
   useEffect(() => {
-    fetch(`${process.env.PUBLIC_URL}/weaponData.json`)
+    fetch(`${process.env.PUBLIC_URL}/weaponData.js`)
       .then((response) => response.json())
       .then((weaponData: Weapon[]) => {
         setWeapons(new Map(weaponData.map((weapon) => [weapon.name, weapon])));
