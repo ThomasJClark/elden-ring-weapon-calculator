c2463a9af994bacf2e5d097d88c8f0026b10c2cc
Tom Clark <the.thomas.j.clark@gmail.com>
2022-12-14 00:33:34 -0500

    Update regulation data to 1.08

--- src/app/useWeapons.tsx
+++ src/app/useWeapons.tsx
@@ -8,7 +8,7 @@ export default function useWeapons() {
   const [weapons, setWeapons] = useState<readonly Weapon[]>([]);
 
   useEffect(() => {
-    fetch(`${process.env.PUBLIC_URL}/weaponData-4.js`)
+    fetch(`${process.env.PUBLIC_URL}/weaponData-5.js`)
       .then((response) => response.json())
       .then((weaponData: EncodedWeapon[]) => {
         setWeapons(weaponData.map(decodeWeapon));
