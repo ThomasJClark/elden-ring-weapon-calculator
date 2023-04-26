f0589790a4a5910b593bdb762618f7b8662820ef
Tom Clark <the.thomas.j.clark@gmail.com>
2023-04-26 00:12:12 -0400

    Add a message about 1.09.1 not being the latest patch, just in case

--- src/app/regulationVersions.tsx
+++ src/app/regulationVersions.tsx
@@ -37,7 +37,12 @@ export interface RegulationVersion {
 
 const regulationVersions: Record<RegulationVersionName, RegulationVersion> = {
   latest: {
-    name: "Patch 1.09.1 (latest)",
+    name: "Patch 1.09",
+    info: (
+      <>
+        Using regulation data from version 1.09 of Elden Ring. Check back soon for the latest patch.
+      </>
+    ),
     affinityOptions,
     fetch: () => fetch(`/regulation-vanilla-v1.09.js?${import.meta.env.VITE_DATA_FORMAT}`),
   },
