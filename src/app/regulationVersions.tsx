2702370ceab27bb071802cc193eddf7581dcd68a
Tom Clark <the.thomas.j.clark@gmail.com>
2023-11-02 22:05:11 -0400

    Update to ERR 0.10.3

--- src/app/regulationVersions.tsx
+++ src/app/regulationVersions.tsx
@@ -53,13 +53,13 @@ const regulationVersions: Record<RegulationVersionName, RegulationVersion> = {
         >
           Elden Ring Reforged
         </Link>{" "}
-        mod v0.10.2A
+        mod v0.10.3
       </>
     ),
     affinityOptions: reforgedAffinityOptions,
     disableTwoHandingAttackPowerBonus: true,
     ineffectiveAttributePenalty: 0.5,
-    fetch: () => fetch(`/regulation-reforged-v0.10.2A.js?${import.meta.env.VITE_DATA_FORMAT}`),
+    fetch: () => fetch(`/regulation-reforged-v0.10.3.js?${import.meta.env.VITE_DATA_FORMAT}`),
   },
   convergence: {
     name: "The Convergence (mod)",
