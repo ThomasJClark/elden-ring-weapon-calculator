b47d558e4f9f2e5a1f5d0b1e22f54c934a632272
Tom Clark <the.thomas.j.clark@gmail.com>
2024-03-17 00:02:42 -0400

    Update to The Convergence 1.4.1.2

--- src/app/regulationVersions.tsx
+++ src/app/regulationVersions.tsx
@@ -78,13 +78,13 @@ const regulationVersions: Record<RegulationVersionName, RegulationVersion> = {
         >
           The Convergence Mod
         </Link>{" "}
-        v1.4.1
+        v1.4.1.2
       </>
     ),
     affinityOptions: convergenceAffinityOptions,
     maxUpgradeLevel: maxSpecialUpgradeLevel,
     splitSpellScaling: true,
-    fetch: () => fetch(`/regulation-convergence-v1.4.1b.js?${import.meta.env.VITE_DATA_FORMAT}`),
+    fetch: () => fetch(`/regulation-convergence-v1.4.1.2.js?${import.meta.env.VITE_DATA_FORMAT}`),
   },
 };
 
